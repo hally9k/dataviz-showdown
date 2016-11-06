62ecb4598f0db1403a31084dba2e96ea13a15cf4
hally9k <hally9k@gmail.com>
2016-11-06 17:58:06 +1300

    Second commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import HighchartsDemo from './Highcharts/highcharts-demo-component'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <HighchartsDemo />
+      </div>
+    );
+  }
+}
+
+export default App;
