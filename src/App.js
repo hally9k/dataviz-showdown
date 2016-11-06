9c9a172d25d7198152c3d088c09a6b2f4556e847
hally9k <hally9k@gmail.com>
2016-11-06 19:03:23 +1300

    highcharts working

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
-import HighchartsDemo from './Highcharts/highcharts-demo-component'
+import HighchartsDonut from './Highcharts/highcharts-donut-component'
+import HighchartsStack from './Highcharts/highcharts-stack-component'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <HighchartsDemo />
+        <HighchartsDonut />
+        <HighchartsStack />
       </div>
     );
   }
